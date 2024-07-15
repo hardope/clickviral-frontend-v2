10066a7f2e2de6863c7a24b54ab5deac561bdb20
hardope <adeyeriopeoluwa05@gmail.com>
2024-07-15 09:25:43 +0100

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react"
+import api from "../api";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+	const [userData, setUserData] = useState<any>(null);
+
+
+	useEffect(() => {
+		const user = () => {
+			api.get("/user/me")
+				.then((res) => {
+					setUserData(res.data.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		};
+
+		user();
+	}, []);
+
+
+	return (
+		<>
+			{
+				userData ? (
+					<div>
+						<h1>Welcome {userData.email}</h1>
+						<p>Your ID is {userData.id}</p>
+					</div>
+				) : (
+					<div>
+						<h1>Welcome</h1>
+						<p>Loading...</p>
+					</div>
+				)
+			}
+		</>
+	)
 }
 
 export default Home
