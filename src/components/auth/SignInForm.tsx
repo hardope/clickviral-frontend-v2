29289b3715cd7f814cd3d31576325bf71daf2970
hardope <adeyeriopeoluwa05@gmail.com>
2024-07-19 10:22:53 +0100

    Account Inactive

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -34,22 +34,13 @@ const SignInForm = () => {
 
     const handleSignIn = (e: React.FormEvent) => {
         e.preventDefault();
+        const signin = e;
         setLoader(true);
         let req = api.post("/user/login/", { email, password });
 
         req.then((res) => {
             try {
-                console.log(res.data.status);
-                if (res.data.status == "unauthorized"){
-                    Notify("Invalid email or password", "error", "Error");
-                    setLoader(false);
-                    return;
-                } else if (res.data.status == "unauthorized-inactive") {
-                    Notify("Account is inactive", "error", "Error");
-                    setLoader(false);
-                    // Initiate account activation - Todo
-                    return;
-                } else if (res.data.status == "success - otp") {
+                if (res.data.status == "success - otp") {
                     Notify("OTP sent to email", "success", "Success");
                     let maxKey = Math.max(...popups.map((popup: any) => popup.key));
                     let signInPopup = (
@@ -131,8 +122,114 @@ const SignInForm = () => {
         }).catch((err) => {
             try {
                 const error = err.response.data;
-                console.log(error);
                 Notify(error.message, "error", "Error");
+                if (error.status == "unauthorized-inactive") {
+                    let user_id = '';
+
+                    api.post("/user/find-account/", { email }).then((res) => {
+
+                        try {
+                            if (res.data.status == "success") {
+                                user_id = res.data.data;
+
+
+                                console.log("Sending verification email " + user_id);
+
+                                api.post(`/user/send-verification-email/${user_id}`, { email }).then((res) => {
+                                    try {
+                                        if (res.data.status != "success") {
+                                            console.log(res.data);
+                                            Notify("An error occurred", "error", "Error");
+                                            setLoader(false);
+                                        }
+                                        Notify("Check your email for account activation", "info", "Info");
+                                    } catch {
+                                        console.log(res.data);
+                                        Notify("An error occurred", "error", "Error");
+                                        setLoader(false);
+                                    }
+                                });
+
+                                let maxKey = Math.max(...popups.map((popup: any) => popup.key));
+                                let activatePopup = (
+                                    <Popup
+                                        key={maxKey + 1}
+                                        title="Activate Account"
+                                        inputs={[{ type: "text", name: "otp", placeholder: "OTP" }]}
+                                        submit="Activate"
+                                        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+                                            e.preventDefault();
+                                            const formData = new FormData(e.target as HTMLFormElement);
+                                            let otp = formData.get("otp") as string;
+                                            if (!otp || otp.length != 6) {
+                                                Notify("Invalid OTP", "error", "Error");
+                                                return;
+                                            }
+                                            setLoader(true);
+                                            let req = api.post(`/user/verify/${user_id}`, { otp });
+                                            req.then((res) => {
+                                                try {
+                                                    if (res.data.status == "unauthorized") {
+                                                        Notify("Invalid OTP", "error", "Error");
+                                                        setLoader(false);
+                                                        return;
+                                                    } else if (res.data.status == "success") {
+                                                        Notify("Account activated", "success", "Success");
+                                                        setLoader(false);
+                                                        handleSignIn(signin);
+                                                    } else {
+                                                        console.log(res.data);
+                                                        Notify(res.data.message, "error", "Error");
+                                                        setLoader(false);
+                                                    }
+                                                } catch {
+                                                    console.log(res.data);
+                                                    Notify("An error occurred", "error", "Error");
+                                                    setLoader(false);
+                                                }
+                                            }).catch((err) => {
+                                                try {
+                                                    const error = err.response.data;
+                                                    console.log(error);
+                                                    Notify(error.message, "error", "Error");
+                                                } catch {
+                                                    console.log(err);
+                                                    Notify("An error occurred", "error", "Error");
+                                                }
+                                                setLoader(false);
+                                            });
+                                        }}
+                                    />
+                                );
+
+                                setPopups([...popups, activatePopup]);
+
+
+                            } else {
+                                console.log(res.data);
+                                Notify("An error occurred", "error", "Error");
+                                setLoader(false);
+                            }
+                        } catch {
+                            console.log(res.data);
+                            Notify("An error occurred", "error", "Error");
+                            setLoader(false);
+                        }
+                    }, (err) => {
+                        try {
+                            const error = err.response.data;
+                            console.log(error);
+                            Notify(error.message, "error", "Error");
+                        } catch {
+                            console.log(err);
+                            Notify("An error occurred", "error", "Error");
+                        }
+                        setLoader(false);
+                    });
+                    setLoader(false);
+                    // Initiate account activation - Todo
+                    return;
+                }
             } catch {
                 console.log(err);
                 Notify("An error occurred", "error", "Error");
