bdfdca3c14619e734e35291215e016f59e2185a4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2024-12-06 18:30:38 +0100

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ACCESS_TOKEN } from './environment';
 
 export const api = axios.create({
-	baseURL: 'https://c-backend.hardope.tech/',
+	baseURL: 'https://backend.click-viral.tech/',
 	headers: {
 		'Content-Type': 'application/json',
 	},
